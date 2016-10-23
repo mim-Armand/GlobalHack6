981d75a7a8114b38d2c7ead74ba2be19b9a348c7
Anthony Howell <anthonyrhowell@gmail.com>
2016-10-23 05:08:50 -0500

    fixed nginx config

--- shelterhub/run.sh
+++ shelterhub/run.sh
@@ -2,12 +2,7 @@
 set -e
 set -x
 
-export NODE_ENV="${NODE_ENV:-development}"
 
-if [ $NODE_ENV != "production" ]; then
-  npm run dev
-else
   mkdir -p $ROOT/logs/nginx
   nginx -g 'daemon off;' -c $ROOT/src/nginx.conf
   nginx -c $ROOT/src/nginx.conf
-fi
